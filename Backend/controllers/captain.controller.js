49cdffa7261224664216ecd6152dc0adbd6c6473
MirAb-77 <abdullahimran017@gmail.com>
2025-02-09 21:21:20 +0500

    Captain login & logout

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -36,7 +36,54 @@ const registerCaptain = async (req, res, next) => {
 
 
 
+const loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const captain = await captainModel.findOne({ email }).select('+password');
+
+    if (!captain) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await captain.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = captain.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, captain });
+}
+
+const getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({ captain: req.captain });
+}
+
+
+
+const logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+
+    await blackListTokenModel.create({ token });
+
+    res.clearCookie('token');
+
+    res.status(200).json({ message: 'Logout successfully' });
+}
+
+
 // ✅ Ensure all functions are correctly exported
 module.exports = {
-    registerCaptain
+    registerCaptain,
+    loginCaptain,
+    getCaptainProfile,
+    logoutCaptain
 };
